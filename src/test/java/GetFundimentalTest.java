55d075d29f3e67710eb0f8ef8b8534bacf93471d
JonoAugustine <jaugust8@jhu.edu>
2018-07-10 19:26:11 -0400

    cleaning

--- src/test/java/GetFundimentalTest.java
+++ src/test/java/GetFundimentalTest.java
@@ -1,9 +1,9 @@
 import com.ampro.robinhood.endpoint.fundamentals.data.TickerFundamentalElement;
-import com.ampro.robinhood.endpoint.fundamentals.data.TickerFundimentalElementList;
+import com.ampro.robinhood.endpoint.fundamentals.data
+        .TickerFundimentalElementList;
 import com.ampro.robinhood.endpoint.fundamentals.methods.GetTickerFundamental;
-import com.ampro.robinhood.endpoint.fundamentals.methods.GetTickerFundimentalList;
-import com.ampro.robinhood.endpoint.instrument.data.InstrumentElementList;
-import com.ampro.robinhood.endpoint.instrument.methods.GetInstrumentByTicker;
+import com.ampro.robinhood.endpoint.fundamentals.methods
+        .GetTickerFundimentalList;
 import com.ampro.robinhood.net.request.RequestManager;
 import com.ampro.robinhood.throwables.RequestTooLargeException;
 import com.ampro.robinhood.throwables.RobinhoodApiException;
