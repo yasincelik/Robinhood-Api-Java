7ce8d0648ed27287a8232899a1b5de3e0783a34e
Mark Cloutier <mark.l.cloutier@gmail.com>
2018-07-17 07:38:33 -0500

    Refactoring ratings request to use Ticker collection response; Adding unit tests

---
+++ src/test/java/com/ampro/robinhood/endpoint/ratings/RatingsTest.java
@@ -0,0 +1,63 @@
+package com.ampro.robinhood.endpoint.ratings;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.ampro.robinhood.BaseTest;
+import com.ampro.robinhood.endpoint.quote.data.TickerQuoteElement;
+import com.ampro.robinhood.endpoint.ratings.data.RatingElementList;
+import com.ampro.robinhood.throwables.RobinhoodApiException;
+
+public class RatingsTest extends BaseTest {
+
+	@Test
+	public void getRatingsByTicker() throws RobinhoodApiException {
+		RatingElementList ratings = api.getRatingsByTickers(tenTickers.get(0));
+		System.err.println(ratings);
+		assertEquals(1, ratings.getResults().size());
+		ratings.getResults().forEach(Assert::assertNotNull);
+	}
+
+	@Test
+	public void getRatingsByTickers() throws RobinhoodApiException {
+		RatingElementList ratings = api.getRatingsByTickers(tenTickers.toArray(new String[10]));
+		System.err.println(ratings);
+		assertEquals(10, ratings.getResults().size());
+		ratings.getResults().forEach(Assert::assertNotNull);
+	}
+
+	@Test
+	public void getRatingsByInstrument() throws RobinhoodApiException {
+
+		TickerQuoteElement quote = api.getQuoteByTicker(tenTickers.get(0));
+
+		RatingElementList ratings = api.getRatingsByInstrumentIds(quote.getInstrumentId());
+
+		System.err.println(ratings);
+		assertEquals(1, ratings.getResults().size());
+		ratings.getResults().forEach(Assert::assertNotNull);
+	}
+
+	@Test
+	public void getRatingsByInstruments() throws RobinhoodApiException {
+
+		List<TickerQuoteElement> quoteList = api.getQuoteListByTickers(tenTickers);
+
+		List<String> instrumentIds = new ArrayList<String>();
+
+		for (TickerQuoteElement quoteElement : quoteList) {
+			instrumentIds.add(quoteElement.getInstrumentId());
+		}
+
+		RatingElementList ratings = api.getRatingsByInstrumentIds(instrumentIds.toArray(new String[10]));
+		System.err.println(ratings);
+		assertEquals(10, ratings.getResults().size());
+		ratings.getResults().forEach(Assert::assertNotNull);
+	}
+
+}
